d0ae32d8719f241cac956a9ac51f91ce701ed51a
Thiago Andrés García González <thiago.garcia@correo.ucu.edu.uy>
2025-11-16 15:09:24 -0300

    cambios nav

--- obligatorioBDD/src/components/Notification.jsx
+++ obligatorioBDD/src/components/Notification.jsx
@@ -1,9 +1,30 @@
-export default function NotificationUser({name, date}) {
+import patchAcceptRequestService from '../service/patchAcceptRequestService'
+import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export default function NotificationUser({name, date, id, onAccepted}) {
   const formattedDate = new Date(date).toLocaleDateString('es-ES', {
     day: '2-digit',
     month: '2-digit',
     year: 'numeric',
   })
+
+  const acceptGroupRequest = async (id) => {
+    try {
+      const result = await patchAcceptRequestService(id)
+
+      if (result?.success) {
+        toast.success('Solicitud aceptada correctamente')
+
+        if (onAccepted) onAccepted()
+      } else {
+        toast.error(result?.description || 'Error al aceptar la solicitud')
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error('Error del servidor')
+    }
+  }
 
   return (
     <div className="flex items-center justify-between rounded-lg px-3 py-2">
@@ -11,7 +32,6 @@ export default function NotificationUser({name, date}) {
         <span className="text-xs text-gray-500">Solicitud de grupo</span>
         <span className="text-sm font-semibold text-gray-800">{name}</span>
 
-        {/* Aquí mostramos la fecha formateada */}
         <span className="text-[11px] text-gray-400">{formattedDate}</span>
       </div>
 
@@ -20,7 +40,9 @@ export default function NotificationUser({name, date}) {
           <i className="fa-solid fa-circle-info text-lg"></i>
         </button>
 
-        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-emerald-100 text-emerald-600 hover:bg-emerald-200 transition">
+        <button
+          className="w-8 h-8 flex items-center justify-center rounded-full bg-emerald-100 text-emerald-600 hover:bg-emerald-200 transition"
+          onClick={() => acceptGroupRequest(id)}>
           <i className="fa-solid fa-check text-lg"></i>
         </button>
       </div>
