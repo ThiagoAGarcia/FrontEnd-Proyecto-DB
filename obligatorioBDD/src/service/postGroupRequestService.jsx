d2e4c8cb8d3eaff88198eea159f9b7a25eea45d7
Agostina Etchebarren <agostina.etchebarren@correo.ucu.edu.uy>
2025-11-19 22:48:28 -0300

    first

--- obligatorioBDD/src/service/postGroupRequestService.jsx
+++ obligatorioBDD/src/service/postGroupRequestService.jsx
@@ -8,7 +8,7 @@ export default async function postGroupRequestService(BODY) {
             headers: {"Content-Type":"application/json"},
             body: JSON.stringify(BODY)
         });
-    if (!res.ok) throw new Error(`POST ${path} -> ${res.status}`);
+    if (!res.ok) throw new Error(`POST ${PATH} -> ${res.status}`);
     const postGroupRequest = await res.json();
     return postGroupRequest;
     } catch(error) {
